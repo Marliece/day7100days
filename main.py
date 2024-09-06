a484bdf9fb7622f8dece302bb359d2def4a898f8
Marliece <180341631+Marliece@users.noreply.github.com>
2024-09-06 16:24:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("🕵️‍♂️ Fake fan Finder 👀")
+print("----------------------")
+anime = input("What's your favourite Anime? ")
+if anime == "One piece":
+  print()
+  character = input("Oh really?! Name me any of the characters? ")
+  if character == "Nami":
+    print()
+    job = input("You got that by pure chance. Okay then, what is her job on the ship? ")
+    if job == "Navigator":
+      print()
+      bounty = input("Hmph! What was her first bounty then? ")
+      if bounty == "Ummm...":
+        print()
+        print("See! FAKE One piece fan!")
+      else:
+        print()
+        print("See! FAKE One piece fan!")
+    else:
+      print()
+      print("See! FAKE One piece fan!")
+  else:
+    print()
+    print("See! FAKE One piece fan!")
+else:
+  print("Ask me about another show")
